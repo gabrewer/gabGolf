0dfbdc141e03da0166dd999a7ba1bb9ab759fc04
Greg Brewer <greg@gabrewer.com>
2017-03-21 22:17:48 -0700

    Initial Checkin

---
+++ gabGolf.Mobile/gabGolf.Mobile/gabGolf.Mobile/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gabGolf.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnNewReoundClicked(object o, EventArgs e)
+        {
+            Navigation.PushAsync(new NewRoundPage());
+        }
+
+        public void OnScoresClicked(object o, EventArgs e)
+        {
+            Navigation.PushAsync(new ScoresPage());
+        }
+    }
+}
---
+++ gabGolf.Mobile/gabGolf.Mobile/gabGolf.Mobile/Models/Round.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gabGolf.Mobile.Models
+{
+    public class Round
+    {
+        public string Course { get; set; }
+        public DateTime PlayedAt { get; set; }
+
+    }
+}
